fixed theoretical SGP signal computation

SimulationResults.py
@@ -6,6 +6,7 @@ class SimulationResults():
     def __init__(self, startingPositions, particles):
         self.startingPositions = startingPositions
         self.particles = particles
+        self.nPart = len(self.particles)
 
         self.displacements = None
         self.endPositions = None
@@ -40,7 +41,9 @@ class SimulationResults():
                 if disp[p][0] > 0:
                     res += 2*np.cos(np.dot(disp[p], qVector))*self.particles[p].getSignal()
         else:
-            for p in range(self.nPart):
-                res += cmath.exp(1j*np.dot(disp[p], qVector))*self.particles[p].getSignal()
+            #for p in range(self.nPart):
+            #    res += cmath.exp(1j*np.dot(disp[p], qVector))*self.particles[p].getSignal()
+            T2Signal = [p.getSignal() for p in self.particles]
+            res = np.dot( np.exp(1j*np.matmul(disp, qVector)), T2Signal)
         return abs(res)/self.nPart
         #return self.signal

Util.py
@@ -2,7 +2,9 @@ import numpy as np
 import random
 import math
 import scipy.stats
-import scipy.integrate
+import scipy.special
+import scipy.optimize
+import scipy.signal
 
 class Util():
 
@@ -42,11 +44,20 @@ class Util():
 
     def genAlphaList(nTerms, nIter, a):
         """positive roots (solving for alpha) of a*alpha = tan(a*alpha)"""
-        alphaList = [(2*k + 1)*np.pi/2 for k in range(nTerms)]
+        alphaList = [(2*k + 1)*np.pi/2 for k in range(nTerms+1)]
         for j in range(nIter):
-            for k in range(nTerms):
+            for k in range(nTerms+1):
                 alphaList[k] = np.arctan(alphaList[k]) + k*np.pi
         return (np.array(alphaList)/a)[1:]
+
+    def genBetaList(nTerms, nIter):
+        """positive roots (solving for beta) of beta = tan(beta)"""
+        betaList = [(2*k + 1)*np.pi/2 for k in range(nTerms)]
+        for j in range(nIter):
+            for k in range(nTerms):
+                betaList[k] = np.arctan(betaList[k]) + k*np.pi
+        betaList[0] = 0
+        return np.array(betaList)
 
     def getPDF_sphere(D, diffusionTime, radius, nTerms, nIter):
         alphaList = Util.genAlphaList(nTerms, nIter, radius)
@@ -67,8 +78,62 @@ class Util():
                                   (np.sin(alpha*r) - alpha*r*np.cos(alpha*r))/(alpha*np.sin(alpha*radius)**2) for alpha in alphaList], axis=0)/radius )
         return F
 
-    def getSignal_sphere(radius):
+    def getSignal_sphere_inf(radius):
         def s(q):
             qR = q*radius
             return 9*(qR*np.cos(qR) - np.sin(qR))**2 / qR**6
+        return s
+
+    def getSignal_sphere_fin(radius, D, t, nNumber, zeroNumber):
+        jpZeros = Util.Jnp_zeros(nNumber,zeroNumber)
+        def s(q):
+            qR = q*radius
+            return (3*scipy.special.spherical_jn(1, qR))**2 / qR**2 \
+                   + 6*qR**2*np.sum([scipy.special.spherical_jn(n, qR, derivative=True)**2 * \
+                    np.sum([(2*n + 1)*alphaNM**2 * np.exp(-alphaNM**2 * D * t / radius**2)/ ( (alphaNM**2 - n**2 - n) * (alphaNM**2 - qR**2)**2)
+                            for alphaNM in jpZeros[n] ], axis=0) for n in range(nNumber) ], axis=0)
+        return s
+
+    def getSignal_sphere_red(tStar, nNumber, zeroNumber):
+        jpZeros = Util.Jnp_zeros(nNumber,zeroNumber)
+        def s(qStar):
+            qR = np.pi*qStar
+            return (3*scipy.special.spherical_jn(1, qR))**2 / qR**2 + 6*qR**2 * np.sum([(scipy.special.spherical_jn(n, qR, derivative=True))**2 * \
+                    np.sum([(2*n + 1)*alphaNM**2 * np.exp(-alphaNM**2 * 4*tStar) / ( (alphaNM**2 - n**2 - n)*(alphaNM**2 - qR**2)**2) \
+                            for alphaNM in jpZeros[n]], axis=0) for n in range(nNumber)], axis=0)
+        return s
+
+    def Jnp(r,n):
+        """derivative of the spherical bessel function of order n"""
+        return scipy.special.spherical_jn(n, r, derivative=True)
+
+    def Jnp_zeros(n,nt):
+        """calculate the zeros of the derivative of the spherical bessel function of order n"""
+        zerosj = np.zeros((n+1, nt))
+        zerosj[0] = Util.genBetaList(nt+1, nt)[1:]
+        points = Util.genBetaList(nt+n, nt)
+        points[0] = 0.01
+        racines = np.zeros(nt+n)
+        for i in range(1,n+1):
+            for j in range(nt+n-i):
+                foo = scipy.optimize.brentq(Util.Jnp, points[j], points[j+1], (i,))
+                racines[j] = foo
+            points = racines
+            zerosj[i][:nt] = racines[:nt]
+        return (zerosj)
+
+    def getSignal_cylinder_red(tStar, nNumber, zeroNumber):
+        def s(qStar):
+            qR = np.pi*qStar
+            kronecker = scipy.signal.unit_impulse(nNumber)
+            return (2*scipy.special.jv(1, qR))**2 / qR**2 + 8*qR**2 * np.sum([(scipy.special.jvp(n, qR))**2 / (1 + kronecker[n]) * \
+                    np.sum([alphaNM**2 * np.exp(-alphaNM**2 *4*tStar) / ( (alphaNM**2 - n**2) * (alphaNM**2 - qR**2)**2) \
+                            for alphaNM in scipy.special.jnp_zeros(n, zeroNumber)], axis=0) for n in range(nNumber)], axis=0)
+        return s
+
+    def getSignal_plane_red(tStar, nNumber):
+        def s(qStar):
+            qa = 2*np.pi*qStar
+            return 2*(1-np.cos(qa))/qa**2 + 4*qa**2 * \
+                   np.sum([(1-(-1)**n * np.cos(qa) ) * np.exp(-(n*np.pi)**2 * tStar) / ((n*np.pi)**2 - qa**2)**2 for n in range(1,nNumber+1)], axis=0)
         return s

Validation.py
@@ -52,8 +52,8 @@ class Validation():
 
         #kolmogorov-smirnov
         print(printMessage)
-        test = scipy.stats.kstest(sim.getDistances(), trueCDF)
         print("Computation time: {compTime}s".format(compTime = time.time() - startTime))
+        test = scipy.stats.kstest(sim.getDistances(), trueCDF)
         print("Kolmogorov-Smirnov test Statistic:", test.statistic)
         print("Kolmogorov-Smirnov test pvalue:", test.pvalue, "\n")
 
@@ -236,3 +236,71 @@ class Validation():
                         .format(diffusionTime=diffusionTime, nPart=nPart, n=nStep, radius=radius, r=r+1, nRuns=nRuns)
                     errors[t, i, r] = Validation.run1Sim(sim, printMessage, histogramTitle, trueCDF, pdfPointsX, pdfPointsY, plotHist)
         return errors
+
+    def run1SimSignal(sim, printMessage, histogramTitle, trueCDF, pdfPointsX, pdfPointsY, plotHist, qPoints):
+        startTime = time.time()
+        sim.run(seed=None, calcData=False)
+        sim = sim.getResults()
+        print("number of particles", len(sim.getDistances()))
+        #print("distances", sim.getDistances())
+
+        print(printMessage)
+        print("Computation time: {compTime}s".format(compTime = time.time() - startTime))
+
+        #signal calculations:
+        simulatedSignal = np.empty(len(qPoints))
+        simulatedSignalReal = np.empty(len(qPoints))
+        print("Average displacement vector:", sim.getAvgDisplacements())
+        for q in range(len(qPoints)):
+            qVec = np.array([qPoints[q], 0, 0])
+            simulatedSignal[q] = sim.getSGPSignal(qVec, real=False)
+            simulatedSignalReal[q] = sim.getSGPSignal(qVec, real=True)
+        return np.array([simulatedSignal, simulatedSignalReal])
+
+    def convSignalFinalPlot(signals, diffusionTimes, radius, D, qPoints, plotTitle):
+        averageSignals = np.average(signals, axis=1)
+        #colors = cm.rainbow(np.linspace(0, 1, len(diffusionTimes)))
+        theoreticalInfinite = Util.getSignal_sphere_inf(radius)
+        plt.plot(qPoints, theoreticalInfinite(qPoints), color="black")
+        for t in range(len(diffusionTimes)):
+            dt = diffusionTimes[t]
+            theoreticalSignal = Util.getSignal_sphere_fin(radius, D, dt, 10, 10)
+            simulatedSignal = averageSignals[t, 0, :]
+            simulatedSignalReal = averageSignals[t, 1, :]
+            plt.plot(qPoints, theoreticalSignal(qPoints), color="r")#colors[t], marker=".")
+            plt.plot(qPoints, simulatedSignal, color="g")#colors[t], marker="o")
+            plt.plot(qPoints, simulatedSignalReal, color="b")#colors[t], marker="v")
+        plt.legend(["Theoretical signal for an infinite diffusion time", "Theoretical signal", "Simulated signal", "Simulated signal (2x real part)"])
+        plt.xlabel("q=gamma*G*delta [um-1]")
+        plt.ylabel("Signal attenuation")
+        plt.title(plotTitle)
+        plt.yscale("log")
+        plt.show()
+
+    def runSphereSignalConv(nRuns, plotHist, diffusionTimes, nStep, nPart, radius, D, T2, qPoints):
+        signals = np.zeros((len(diffusionTimes), nRuns, 2, len(qPoints)))
+
+        for t in range(len(diffusionTimes)):
+            diffusionTime = diffusionTimes[t]
+            truePDF = Util.getPDF_sphere(D, diffusionTime, radius, 500, 5)
+            trueCDF = Util.getCDF_sphere(D, diffusionTime, radius, 1000, 5)
+
+            pdfPointsX = np.linspace(0, 1.1*radius, 500)
+            pdfPointsY = [truePDF(p) for p in pdfPointsX]
+
+            for r in range(nRuns):
+                diffusionTime = diffusionTimes[t]
+                timeStep = diffusionTime / nStep
+                l = (6*D*timeStep)**0.5
+                envSize = 5*radius
+                env = Environment(T2, D, envSize, envSize, envSize)
+                part = [Particle3D(*Util.getRandomDirection()*Util.getRandomQuadratic(radius)) for i in range(nPart)]
+                #part = [Particle3D(0, 0, 0) for i in range(nPart)]
+                sim = Simulation(nStep, timeStep, part, env, [Sphere(0, 0, 0, T2, D, 0, radius)])
+                printMessage = "{diffusionTime}ms diffusion time ({t}/{totDt}), run {r}/{nRuns}:"\
+                    .format(diffusionTime=diffusionTime, t=t+1, totDt=len(diffusionTimes), r=r+1, nRuns=nRuns)
+                histogramTitle = "Probability distribution of the distance travelled by a particle (sphere radius = {radius}um)\n"\
+                                 "Diffusion time = {diffusionTime}ms, Number of particles = {nPart}, Number of steps = {n}, run {r}/{nRuns}"\
+                    .format(diffusionTime=diffusionTime, nPart=nPart, n=nStep, radius=radius, r=r+1, nRuns=nRuns)
+                signals[t, r, :, :] = Validation.run1SimSignal(sim, printMessage, histogramTitle, trueCDF, pdfPointsX, pdfPointsY, plotHist, qPoints)
+        return signals

_test_signal.py
@@ -1,38 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.special
 
 from Util import Util
 
-"""
-points = np.linspace(0, 1.5, 500)[1:]
+radius = 8
+nNumber = 4
+nt = 5
+pointsTest = np.linspace(0, 21, 500)[1:]
 
-#def f(qR):
-#    return 9*(qR*np.cos(qR) - np.sin(qR))**2 / qR**6
-
-f = Util.getSignal_sphere(8)
-
-plt.plot(points, f(points))
-plt.yscale("log")
-plt.show()
-"""
-'''
-x = [0, 1, 2]
-y = x
-yerr = [1, 1, 1]
-plt.errorbar(x, y, yerr, color="red", ecolor="green")
+for n in range(nNumber):
+    plt.plot(pointsTest, scipy.special.spherical_jn(n, pointsTest, derivative=True))
+#plt.plot(pointsTest, scipy.special.spherical_jn(n, pointsTest, derivative=False), color="red")
+#xPoints = (2.*np.arange(1,nt+n+1)-1)*np.pi/2
+plt.legend(list(range(nNumber)))
+xPoints2 = np.arange(1,nt+nNumber+1)*np.pi
+xPoints = Util.genAlphaList(nt, nt, radius)*radius
+plt.scatter(xPoints, np.zeros(len(xPoints)), color="black")
+#plt.scatter(xPoints2, np.zeros(len(xPoints2)), color="red")
 plt.grid()
 plt.show()
-'''
 
-
-D = 2
-diffusionTime = 2
+points = np.linspace(0, 1.5, 500)[1:]
 radius = 8
-print("start")
-truePDF = Util.getPDF_sphere(D, diffusionTime, radius, 500, 5)
-trueCDF = Util.getCDF_sphere(D, diffusionTime, radius, 500, 5)
+D = 2
+t = 20
+nNumber = 10
+zeroNumber = 10
 
-pdfPointsX = np.linspace(0, 1.1*radius, 500)
-#pdfPointsY = [truePDF(p) for p in pdfPointsX]
-pdfPointsY = truePDF(pdfPointsX)
-print("done")
+qStarPoints = np.linspace(0, 3.6, 500)[1:]
+for tStar in [0.05, 0.1, 0.2, 0.5, 1]:
+    fStar = Util.getSignal_sphere_red(tStar, nNumber, zeroNumber)
+    #fStar = Util.getSignal_cylinder(tStar, nNumber, zeroNumber)
+    #fStar = Util.getSignal_plane(tStar, 100)
+    plt.plot(qStarPoints, fStar(qStarPoints))
+plt.yscale("log")
+plt.grid()
+plt.show()
+
+
+f = Util.getSignal_sphere_inf(radius)
+g = Util.getSignal_sphere_fin(radius, D, t, nNumber, zeroNumber)
+#h = Util.getSignal_sphere_2(radius, D, t, 10*nNumber, 10*zeroNumber)
+
+plt.plot(points, f(points), color="red")
+plt.plot(points, g(points), color="green")
+#plt.plot(points, h(points), color="blue")
+plt.yscale("log")
+plt.show()